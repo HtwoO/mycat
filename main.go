189720fa761ed897a057565c6abb2e2bc8db85a5
Fonzie Huang <hxf.prc+web@gmail.com>
2022-04-17 19:20:16 +0800

    switch statement and function example

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "os"
 
 func main() {
-	println(os.Args[0])
+	switch len(os.Args) {
+	case 1: // no argument provided
+		usage()
+	}
+}
+
+func usage() {
+	println(os.Args[0], "Usage 使用说明")
 }
